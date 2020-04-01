664c0dee26babfb2fd900d4859c7ce7cc249bf17
cfranklin <franklin.carson@gmail.com>
2020-04-01 08:22:09 -0600

    Finished the Contact component.

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,8 +1,69 @@
 <template>
     <div>
-        <v-overlay v-if="shown">
-            I'm excited to hear from you
-            Phone: (801) 669 - 3727
+        <v-overlay v-if="shown"
+                   z-index="6"
+        >
+            <v-card light
+                    min-width="450">
+                <v-row class="pt-3 pr-5">
+                    <v-spacer/>
+                    <v-btn icon
+                           @click="closeButtonClicked">
+                        <v-icon>
+                            mdi-close
+                        </v-icon>
+                    </v-btn>
+                </v-row>
+                <div class="pr-10 pl-10 pb-10">
+                    <span class="display-1 pl-2">
+                        Contact Me
+                    </span>
+<!--                    <v-card-subtitle>-->
+<!--                        Ward Employment Specialist-->
+<!--                    </v-card-subtitle>-->
+
+                    <v-row class="pa-3 pt-6">
+                        <v-col>
+                            <v-chip
+                                    label
+                                    color="highlight"
+                                    dark
+                            >
+                                <v-icon left>
+                                    mdi-phone
+                                </v-icon>
+                                Phone:
+                            </v-chip>
+                        </v-col>
+                        <v-col>
+                            <div class="pt-1 text-end">
+                                (801) 669-3727
+                            </div>
+                        </v-col>
+
+                    </v-row>
+                    <v-row class="pa-3">
+                        <v-col>
+                            <v-chip
+                                    label
+                                    color="highlight"
+                                    dark
+                            >
+                                <v-icon left>
+                                    mdi-email-outline
+                                </v-icon>
+                                Email:
+                            </v-chip>
+                        </v-col>
+                        <v-col>
+                            <div class="pt-1 text-end">
+                                franklin.carson@gmail.com
+                            </div>
+                        </v-col>
+
+                    </v-row>
+                </div>
+            </v-card>
         </v-overlay>
     </div>
 </template>
@@ -16,6 +77,9 @@
         methods: {
             toggleShown: function () {
                 this.shown = !this.shown
+            },
+            closeButtonClicked: function () {
+                this.toggleShown()
             }
         }
     }
